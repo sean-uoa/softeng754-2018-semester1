31ef94e987f79631d6bc479c888698dbae181b3a
Sean-gzcn <xyzhanggz@gmail.com>
2018-05-08 23:06:08 +1200

    Write test case for dollar multiplication

---
+++ src/test/java/nz/ac/auckland/se754sem2018/mycash/DollarMulitplicationTest.java
@@ -0,0 +1,21 @@
+package nz.ac.auckland.se754sem2018.mycash;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class DollarMulitplicationTest {
+
+	@Test
+	public void testMuliplication() {
+		// Preconditions
+		Dollar five = new Dollar(5);
+		
+		// Actions
+		five.times(2);
+		
+		// Postconditions
+		assertEquals(10, five.amount);
+	}
+
+}
