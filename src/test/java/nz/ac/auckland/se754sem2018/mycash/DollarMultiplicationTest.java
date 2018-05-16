4d4a5f5e3b56e21bb4f69f1c67c2b8ed44f93893
Sean-gzcn <xyzhanggz@gmail.com>
2018-05-16 15:49:42 +1200

    add Dollar mock objects examples

--- src/test/java/nz/ac/auckland/se754sem2018/mycash/DollarMultiplicationTest.java
+++ src/test/java/nz/ac/auckland/se754sem2018/mycash/DollarMultiplicationTest.java
@@ -2,10 +2,12 @@ package nz.ac.auckland.se754sem2018.mycash;
 
 import static org.junit.Assert.*;
 
+import org.junit.Ignore;
 import org.junit.Test;
+import org.mockito.Mockito;
 
 public class DollarMultiplicationTest {
-
+	
 	@Test
 	public void testMuliplication() {
 		// Preconditions
@@ -17,5 +19,57 @@ public class DollarMultiplicationTest {
 		// Postconditions
 		assertEquals(10, five.amount);
 	}
+	
+//	@Ignore
+//	@Test
+//	public void testMockObjects() {
+//		// Given
+//		Dollar five = Mockito.mock(Dollar.class);
+//		Mockito.when(five.times(2)).thenReturn(10);
+//		
+//		// When
+//		five.times(2);
+//		
+//		// Then
+//		assertEquals(10, five.amount);
+//	}
+//	
+//	@Ignore
+//	@Test
+//	public void testSpyObjects() {
+//		// Given
+//		Dollar five = Mockito.spy(new Dollar(5));
+//		Mockito.doReturn(10).when(five).times(2);
+//		
+//		// When
+//		five.times(2);
+//		
+//		// Then
+//		assertEquals(10, five.amount);
+//	}
+//	
+//	@Ignore
+//	@Test(expected=Exception.class)
+//	public void testThrowException() {
+//		// Given
+//		Dollar five = Mockito.mock(Dollar.class);
+//		Mockito.when(five.times(2)).thenThrow(new Exception("blabla"));
+//		
+//		// When
+//		five.times(2);
+//	}
+//	
+//	@Ignore
+//	@Test
+//	public void testVerifyStep() {
+//		// Given
+//		Dollar five = Mockito.mock(Dollar.class);
+//		
+//		// When
+//		five.times(2);
+//		
+//		// Then
+//		Mockito.verify(five, Mockito.times(1)).times(1);
+//	}
 
 }
