7acb4403bbb47fcec175ae34cc8eb9b38cd6b8e6
Sean-gzcn <xyzhanggz@gmail.com>
2018-05-20 01:43:06 +1200

    MongoDBServer test for insertion and query

---
+++ src/test/java/nz/ac/auckland/se754sem2018/mycash/integration/MongoDBServerTest.java
@@ -0,0 +1,34 @@
+package nz.ac.auckland.se754sem2018.mycash.integration;
+
+import static org.junit.Assert.*;
+
+import org.bson.Document;
+import org.junit.Test;
+
+import com.mongodb.MongoClient;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoCursor;
+import com.mongodb.client.MongoDatabase;
+
+public class MongoDBServerTest {
+
+	@Test
+	public void shouldConnectToMongoDBServer() {
+		MongoClient mongoClient = new MongoClient("localhost", 27017);
+		MongoDatabase database = mongoClient.getDatabase("myMongoDb");
+		System.out.println(database.getName());
+		
+		database.createCollection("customers");
+		MongoCollection<Document> collection = database.getCollection("customers");
+		Document document = new Document();
+		document.put("name", "xuyun");
+		document.put("company", "UoA");
+		collection.insertOne(document);
+		
+		MongoCursor<Document> cursor = collection.find().iterator();
+		while(cursor.hasNext()) {
+			System.out.println(cursor.next());
+		}
+	}
+
+}
