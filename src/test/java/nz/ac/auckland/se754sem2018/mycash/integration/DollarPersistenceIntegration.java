96e46f6ee91658fb75adb3b8cacdb48535bfa67e
Sean-gzcn <xyzhanggz@gmail.com>
2018-05-20 02:11:58 +1200

    debug: change file names

--- src/test/java/nz/ac/auckland/se754sem2018/mycash/integration/DollarPersistenceIntegration.java
+++ src/test/java/nz/ac/auckland/se754sem2018/mycash/integration/DollarPersistenceIntegration.java
@@ -6,30 +6,31 @@ import org.bson.Document;
 import org.junit.Test;
 
 import com.mongodb.MongoClient;
+import com.mongodb.client.MongoCollection;
 import com.mongodb.client.MongoCursor;
 import com.mongodb.client.MongoDatabase;
 
 import nz.ac.auckland.se754sem2018.mycash.Dollar;
-import nz.ac.auckland.se754sem2018.mycash.DollarPersistence;
 
 public class DollarPersistenceIntegration {
 
 	@Test
 	public void shouldConnectToMongoDBServer() {
 		MongoClient mongoClient = new MongoClient("localhost", 27017);
-		String myCashDBName = "my-cash-db";
-		String myCashCollectionName = "my-cash-collection";
+		MongoDatabase database = mongoClient.getDatabase("my-cash-db");
+		System.out.println(database.getName());
 		
-		Dollar five = new Dollar(5);
-		DollarPersistence dollarPersistence = new DollarPersistence(mongoClient, myCashDBName, myCashCollectionName);
-		dollarPersistence.saveNZD(five);
-		System.out.println("Dollar save action perfomed!");
+//		database.createCollection("customers");
+		MongoCollection<Document> collection = database.getCollection("my-cash-collection");
+		Document document = new Document();
+		document.put("name", (new Dollar(5)).toString());
+		document.put("company", "UoA");
+		collection.insertOne(document);
 		
-		MongoCursor<Document> cursor = dollarPersistence.getCollection().find().iterator();
+		MongoCursor<Document> cursor = collection.find().iterator();
 		while(cursor.hasNext()) {
 			System.out.println(cursor.next());
 		}
 	}
-
 
 }
