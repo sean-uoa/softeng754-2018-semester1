6da06f11fc7e74c4d47cff5f3626a0cf5a10badc
Sean-gzcn <xyzhanggz@gmail.com>
2018-05-19 22:58:13 +1200

    Implement MongoDB client initialization, run green

--- src/main/java/nz/ac/auckland/se754sem2018/mycash/MyCash.java
+++ src/main/java/nz/ac/auckland/se754sem2018/mycash/MyCash.java
@@ -3,12 +3,14 @@ package nz.ac.auckland.se754sem2018.mycash;
 import com.mongodb.client.MongoClient;
 
 public class MyCash {
+	
+	MongoClient mongoClient;
 
 	public MyCash(MongoClient mongoClient) {
-		
+		this.mongoClient = mongoClient;
 	}
 	
 	public boolean isMongoDBClientNull() {
-		return true;
+		return this.mongoClient == null;
 	}
 }
